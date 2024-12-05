ecb717d480eea412822ba008a81cb7a37dfec284
abhijeetGithu <abhijeetas8660211@gmail.com>
2024-12-05 18:43:09 +0530

    color changes

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
         <div className='bg-white'>
             <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
                 <div>
-                    <h1 className='text-2xl font-bold'>Career<span className='text-[#F83002]'>Hive</span></h1>
+                    <h1 className='text-2xl font-bold'>Career<span className='text-[#4169E1]'>Hive</span></h1>
                 </div>
                 <div className='flex items-center gap-12'>
                     <ul className='flex font-medium items-center gap-5'>
